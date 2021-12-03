7b63ec6f27239eb477b637bad4affe3d0cc5db17
sabbas76437 <haider76437@gmail.com>
2021-12-03 01:52:16 -0600

    my example

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,29 +1,46 @@
 import React, { Component } from 'react';
 class Counter extends React.Component {
+ 
+
     state = {
-        count :0,
-        tag:['tag1','tag2','tag3']
+        count :1,
+        tag:['tag1','tag22','tag3']
     };
+    // constructor(){
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    
     styles ={
         fontSize : 100,
         fontWeight: 'bold'
     }
+
+   
     renderTags(){
         if(this.state.tag.length === 0) return <p>No List found</p>
         return <ul>{this.state.tag.map(tag => <li key={tag}>{tag}</li>)}</ul>;
          
     }
 
+
     render() { 
+        let product = {id:1};
        
         return (
             <React.Fragment>
             <span style={{fontSize:30}} className = {this.getCounterBadgeColorChange()} >{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">increment</button>
+            <button onClick = {() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">increment</button>
             {this.renderTags()}
             </React.Fragment>);
     }
-    getCounterBadgeColorChange() {
+    handleIncrement = product =>{
+        console.log(product);
+        this.setState({count : this.state.count +1});
+    }
+
+    getCounterBadgeColorChange() { 
         let classes = "badge m-2 badge-";
         classes += (this.state.count === 0 ? "warning" : "primary");
         return classes;
